fca40107d3daf8eaff6e645da0443cc01c334771
Jose Luis <josediazh1925@gmail.com>
2025-06-26 21:50:52 -0600

    error para la ruta del archivo uiSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productSlice } from './products/productsSlice'
 import { authSlice } from './auth/authSlice'
-import { uiSlice } from './ui/UiSlice'
+import { uiSlice } from './ui/uiSlice'
 
 export const store = configureStore({
     reducer: {
-        ui: uiSlice.reducer,
+        ui: uiSlice,
         calendar: productSlice.reducer,
         auth: authSlice.reducer
     },
 })
+
